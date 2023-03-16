391f86b3d37576294850ef3aa618c9d2c1010456
brazil_ <127901026+brazil-502@users.noreply.github.com>
2023-03-16 06:50:00 +0530

    Add files via upload

---
+++ overwritten.py
@@ -0,0 +1,64 @@
+#overwritter
+#developer: brazil_502
+#version: 1.0
+
+import os
+import time
+
+print("""\
+           ___                               _ _   _                     
+          /___\__   _____ _ ____      ___ __(_) |_| |_ ___ _ __          
+ _____   //  //\ \ / / _ \ '__\ \ /\ / / '__| | __| __/ _ \ '_ \   _____ 
+|_____| / \_//  \ V /  __/ |   \ V  V /| |  | | |_| ||  __/ | | | |_____|
+        \___/    \_/ \___|_|    \_/\_/ |_|  |_|\__|\__\___|_| |_|        """)
+
+path = input("the file name:- ")
+
+if os.path.exists(path):
+    print("that file exists!")
+    time.sleep(0.1)
+    print("reading file.")
+    time.sleep(0.1)
+    print("reading file..")
+    time.sleep(0.1)
+    print("reading file...")
+    time.sleep(0.1)
+    print("reading file..")
+    time.sleep(0.1)
+    print("reading file.")
+    time.sleep(0.1)
+    print("reading file..")
+    time.sleep(0.1)
+    print("reading file...")
+    time.sleep(0.1)
+    print("- FILE CONTENTS -")
+else:
+    print("that file does not exist")
+
+try:
+    with open(path) as file:
+        print(file.read())
+        print("- - - - - -")
+except FileNotFoundError:
+    print(" ")
+
+if os.path.exists(path):
+    with open(path, 'w') as file:
+        file.write(input("enter the override:- "))
+        print("overwriting.")
+        time.sleep(0.1)
+        print("overwriting..")
+        time.sleep(0.1)
+        print("overwriting...")
+        time.sleep(0.1)
+        print("overwriting..")
+        time.sleep(0.1)
+        print("overwriting.")
+        time.sleep(0.1)
+        print("overwriting..")
+        time.sleep(0.1)
+        print("overwriting...")
+        time.sleep(0.1)
+        print("Done!")
+
+
